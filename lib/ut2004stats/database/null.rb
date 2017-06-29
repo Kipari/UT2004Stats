a4bbf2933093ae7b3f3f2d702921686c7e8eaceb
Christoffer Müller Madsen <christoffer@guava.space>
2017-06-29 20:44:56 +0200

    add Null database type (does nothing)

---
+++ lib/ut2004stats/database/null.rb
@@ -0,0 +1,32 @@
+module UT2004Stats
+  module Database
+    class NullDatabase
+      def score ( event )
+      end
+      
+      def new_game ( event )
+      end
+
+      def end_game ( event )
+      end
+      
+      def server_init ( event )
+      end
+      
+      def player_name_change ( event )
+      end
+
+      def special_kill ( event)
+      end
+
+      def kill ( event )
+      end
+
+      def player_connect ( event )
+      end
+
+      def player_string ( event )
+      end
+    end
+  end
+end
