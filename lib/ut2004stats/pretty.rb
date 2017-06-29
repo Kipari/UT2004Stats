55680bffdf95c16e64b631a07e630f5126771d71
Christoffer Müller Madsen <christoffer@guava.space>
2017-06-30 00:00:18 +0200

    add pretty strings

---
+++ lib/ut2004stats/pretty.rb
@@ -0,0 +1,12 @@
+# Multi kills
+module UT2004Stats
+  module Strings
+    SPECIAL_KILLS = { "multikill_1" => "multi-kill",
+                      "multikill_2" => "?",
+                      "multikill_3" => "?",
+                      "multikill_4" => "?",
+                      "first_blood" => "first blood",
+                      "spree_1" => "killing spree" }
+    WEAPONS = { "SuperShockRifle" => "Insta-gib Rifle" }
+  end
+end
