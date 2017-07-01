d24885d92b17d0d3085d932497fe837c9e3cb92a
Christoffer Müller Madsen <christoffer@guava.space>
2017-07-01 03:16:58 +0200

    Add output directory argument for bin/render_html.rb

--- bin/render_html.rb
+++ bin/render_html.rb
@@ -6,11 +6,21 @@ require 'ut2004stats/output/html'
 require 'ut2004stats/pretty'
 
 require 'optparse'
+require 'fileutils'
 
 options = {}
 OptionParser.new do |opts|
   opts.banner = "Usage: render_html.rb [options]"
+
+  opts.on("-oDIRECTORY", "--outputdir=DIRECTORY", "Directory into which to output HTML report") do |d|
+    options[:outdir] = d
+  end
 end.parse!
+
+unless options[:outdir]
+    puts "An output directory must be specified!"
+    exit
+end
 
 parser = UT2004Stats::Parser::OLStats.new
 db = UT2004Stats::Database::Memory.new
@@ -18,4 +28,4 @@ outputter = UT2004Stats::Output::HTML.new
 
 parser.parse( ARGF.read, db )
 
-outputter.output( db, "output/" )
+outputter.output( db, options[:outdir] )
